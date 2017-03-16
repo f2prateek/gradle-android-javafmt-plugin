b444edf34d235c4e3d47c472911ff1efee481353
Gabriel Ittner <gabriel.ittner@gmail.com>
2017-03-16 12:05:12 +0100

    Add ability to configure google-java-format version

--- src/main/groovy/com/f2prateek/javafmt/AndroidJavaFmtExtension.groovy
+++ src/main/groovy/com/f2prateek/javafmt/AndroidJavaFmtExtension.groovy
@@ -1,18 +1,11 @@
 package com.f2prateek.javafmt
 
-import com.google.googlejavaformat.java.JavaFormatterOptions;
-
 class AndroidJavaFmtExtension {
   Style style = Style.GOOGLE
 
+  // Should match the enum `JavaFormatterOptions.Style`.
   enum Style {
-    AOSP(JavaFormatterOptions.Style.AOSP),
-    GOOGLE(JavaFormatterOptions.Style.GOOGLE);
-
-    def value;
-
-    Style(JavaFormatterOptions.Style value) {
-      this.value = value;
-    }
+    AOSP,
+    GOOGLE
   }
 }
--- src/main/groovy/com/f2prateek/javafmt/FormatterFactory.groovy
+++ src/main/groovy/com/f2prateek/javafmt/FormatterFactory.groovy
@@ -1,13 +1,30 @@
 package com.f2prateek.javafmt
 
-import com.google.googlejavaformat.java.Formatter
-import com.google.googlejavaformat.java.JavaFormatterOptions
 import org.gradle.api.Project
 
 class FormatterFactory {
   static formatter(Project project) {
+    def config = project.configurations.getByName(AndroidJavafmtPlugin.CONFIG_NAME)
+    def urls = new ArrayList<URL>()
+    config.files.each { File file ->
+      urls += file.toURI().toURL()
+    }
+
+    // These classes cannot be loaded directly.
+    def cl = new URLClassLoader(urls as URL[]);
+    def optionsClass = cl.loadClass("com.google.googlejavaformat.java.JavaFormatterOptions")
+    def styleClass = cl.loadClass("com.google.googlejavaformat.java.JavaFormatterOptions\$Style")
+    def formatterClass = cl.loadClass("com.google.googlejavaformat.java.Formatter")
+
+    // def optionsBuilder = JavaFormatterOptions.builder()
+    def optionsBuilder = optionsClass.getMethod("builder").invoke(null)
+
     def extension = project.extensions.getByType(AndroidJavaFmtExtension)
-    def options = JavaFormatterOptions.builder().style(extension.style.value).build()
-    return new Formatter(options)
+    optionsBuilder.style(Enum.valueOf(styleClass, extension.style.toString()))
+
+    def options = optionsBuilder.build()
+
+    // return new Formatter(options)
+    return formatterClass.getConstructor(optionsClass).newInstance(options)
   }
 }
