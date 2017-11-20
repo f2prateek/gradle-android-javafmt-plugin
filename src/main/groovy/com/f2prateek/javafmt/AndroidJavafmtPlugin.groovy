4c2e1215d91c42eeb3dae02166c3721c00d8e9c6
Prateek Srivastava <f2prateek@gmail.com>
2017-11-19 21:28:53 -0800

    Remove `fmt` task dependency on `assemble`.
    Closes #19.
    
    Going forwards, you'll have to run `fmt` explicitly. This is more in line with other tools like standard.

--- src/main/groovy/com/f2prateek/javafmt/AndroidJavafmtPlugin.groovy
+++ src/main/groovy/com/f2prateek/javafmt/AndroidJavafmtPlugin.groovy
@@ -41,28 +41,26 @@ class AndroidJavafmtPlugin implements Plugin<Project> {
               "Must be applied with 'android' or 'android-library' plugin.")
     }
 
+    // Setup `fmt` tasks.
     def fmtTasks = []
-    def checkFmtTasks = []
-
     variants.all { variant ->
-      // TODO: Consolidate common code.
-
       def fmt = project.tasks.create "fmt${variant.name.capitalize()}", JavaFmtTask
       fmt.dependsOn variant.javaCompile
       fmt.source variant.javaCompile.source
-      project.tasks.getByName("assemble").dependsOn fmt
       fmtTasks.add fmt
+    }
+    def fmt = project.tasks.create "fmt"
+    fmt.dependsOn fmtTasks
 
+    // Setup `checkFmt` tasks.
+    def checkFmtTasks = []
+    variants.all { variant ->
       def checkFmt = project.tasks.create "checkFmt${variant.name.capitalize()}", CheckFmtTask
       checkFmt.dependsOn variant.javaCompile
       checkFmt.source variant.javaCompile.source
       project.tasks.getByName("check").dependsOn checkFmt
       checkFmtTasks.add checkFmt
     }
-
-    def fmt = project.tasks.create "fmt"
-    fmt.dependsOn fmtTasks
-
     def checkFmt = project.tasks.create "checkFmt"
     checkFmt.dependsOn checkFmtTasks
   }
